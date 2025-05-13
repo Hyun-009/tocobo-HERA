0f98e17074048b12bbe8c7818c84a863e2e01ccd
Hyun-009 <iovemint@naver.com>
2025-05-13 14:56:00 +0900

    Add files via upload

---
+++ components/Section4.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Section4.scss'
+const Section4 = () => {
+    return (
+        <section className="sc4">
+            <div className="inner">
+                <div className="t-wrap">
+                    <div className="star">
+                        <h2 className="con_tit">
+                            BRAND STORY
+                        </h2>
+                        <a href="#" className="con_btn">MEET TOCOBO</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section4
