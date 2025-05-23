e699f7e295cf41b37cc5a9f3fcdc40a682b07ffd
Hyun-009 <iovemint@naver.com>
2025-05-23 15:24:29 +0900

    Add files via upload

--- src/data/utilData.js
+++ src/data/utilData.js
@@ -1,7 +1,7 @@
 const utilData=[
-    {name:'language',link:'#',icon:'/tocobo-HERA/img/icon_language.png'},
-    {name:'search',link:'#',icon:'/tocobo-HERA/img/icon_search.png'},
-    {name:'mypage',link:'#',icon:'/tocobo-HERA/img/icon_myp.png'},
+    {name:'language',link:'#',icon:'./img/icon_language.png'},
+    {name:'search',link:'#',icon:'./img/icon_search.png'},
+    {name:'mypage',link:'#',icon:'./img/icon_myp.png'},
     {
         name:'cart',
         link:'#',
